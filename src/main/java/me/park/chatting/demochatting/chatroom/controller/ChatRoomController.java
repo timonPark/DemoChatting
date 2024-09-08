d50ab7a3eaa75a44d047a76b387c1f9416968966
박 종훈 <m05214@naver.com>
2024-09-08 11:56:28 +0900

    feat: 채팅방 생성 api 데이터 바인딩 방식을 변경함 Request Param에서 Request Body로

--- src/main/java/me/park/chatting/demochatting/chatroom/controller/ChatRoomController.java
+++ src/main/java/me/park/chatting/demochatting/chatroom/controller/ChatRoomController.java
@@ -2,13 +2,10 @@ package me.park.chatting.demochatting.chatroom.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import me.park.chatting.demochatting.chatroom.dto.ChatRoomRequestDto;
 import me.park.chatting.demochatting.chatroom.entity.ChatRoom;
 import me.park.chatting.demochatting.chatroom.service.ChatRoomService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +16,8 @@ public class ChatRoomController {
 
   // 채팅방 생성
   @PostMapping
-  public ChatRoom createRoom(@RequestParam String name) {
-    return chatRoomService.createRoom(name);
+  public ChatRoom createRoom(@RequestBody ChatRoomRequestDto chatRoomRequestDto) {
+    return chatRoomService.createRoom(chatRoomRequestDto.getName());
   }
 
   // 전체 채팅방 조회
---
+++ src/main/java/me/park/chatting/demochatting/chatroom/dto/ChatRoomRequestDto.java
@@ -0,0 +1,8 @@
+package me.park.chatting.demochatting.chatroom.dto;
+
+import lombok.Data;
+
+@Data
+public class ChatRoomRequestDto {
+    private String name;
+}
